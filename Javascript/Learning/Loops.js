0a640545b1f500deeac0dbd9323683bca6ad337d
MujtabaNite <mujtaba.office.mail@gmail.com>
2026-07-29 10:43:59 +0500

    day 2:loops

---
+++ Javascript/Learning/Loops.js
@@ -0,0 +1,30 @@
+for (var i = 1; i<=5; i++){
+    console.log(i);
+}
+console.log("Counting Completed")
+
+
+for (var i = 5; i>=1; i--){
+    console.log(i);
+}
+console.log("Counting Completed")
+
+
+var i = 1
+while(i<=5){
+    console.log(i)
+    i += 1
+}
+console.log("Counting Completed")
+
+var i = 1
+while(i<=5){
+    console.log(i++)
+}
+
+
+var i = 2020
+while(i>= 2015){
+    console.log(i--)
+}
+console.log("Counting Completed")
