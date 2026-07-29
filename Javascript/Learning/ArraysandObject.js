56a6adb76f2a76448bce0095da23f9d962629f50
MujtabaNite <mujtaba.office.mail@gmail.com>
2026-07-29 13:00:10 +0500

    Day 2 : Arrays and Objects

--- Javascript/Learning/ArraysandObject.js
+++ Javascript/Learning/ArraysandObject.js
@@ -21,3 +21,11 @@ favcar.convertible = true;
 
 console.log(favcar)
 
+var car = {};
+
+car.color = "red";
+
+//add a method to the car object so that it can be called as car.turnkey()
+car.turnKey = function() { 
+  console.log('engine running'); 
+}
