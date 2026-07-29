f31f0f663e7e67c875c10455cf1a5f2a2b4caf05
MujtabaNite <mujtaba.office.mail@gmail.com>
2026-07-29 10:56:27 +0500

    Day 2 : Loops

--- Javascript/Learning/Loops.js
+++ Javascript/Learning/Loops.js
@@ -28,3 +28,35 @@ while(i>= 2015){
     console.log(i--)
 }
 console.log("Counting Completed")
+
+for (var i = 1; i<=5; i++){
+    if( i == 1){
+        console.log("Gold Medal")
+    }
+    else if( i == 2){
+        console.log("Silver Medal")
+    }
+    else if( i == 3){
+        console.log("Bronze Medal")
+    }
+    else{
+        console.log(i)
+    }
+}
+
+for (var i = 1; i<=5; i++){
+    switch(i){
+        case 1:
+            console.log("Gold Medal")
+            break;
+        case 2:
+            console.log("Silver Medal")
+            break;
+        case 3:
+            console.log("Bronze Medal")
+            break;
+        default:
+            console.log(i)
+    }
+
+}
