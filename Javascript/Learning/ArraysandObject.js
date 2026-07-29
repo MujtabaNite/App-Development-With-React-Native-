64d89376ba0153b7df6eda724269bb9e3024ffc8
MujtabaNite <mujtaba.office.mail@gmail.com>
2026-07-29 12:38:28 +0500

    Day 2 : Array and objects

---
+++ Javascript/Learning/ArraysandObject.js
@@ -0,0 +1,23 @@
+var clothes = []
+clothes.push("T-shirt");
+clothes.push("Jeans");
+clothes.push("Jacket");
+clothes.push("Sweater");
+clothes.push("Shorts");
+
+
+
+clothes.pop();
+
+clothes.push("Socks");
+
+console.log(clothes.at(2))
+console.log(clothes[2])
+
+var favcar = {}
+
+favcar.color = "Red";
+favcar.convertible = true;
+
+console.log(favcar)
+
