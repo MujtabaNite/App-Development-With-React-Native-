a5908d26d2a89b675bdaf5aec89b02d5a9c2a2ee
MujtabaNite <mujtaba.office.mail@gmail.com>
2026-07-30 14:24:21 +0500

    Day 3: Oops

--- Javascript/Learning/Oops.js
+++ Javascript/Learning/Oops.js
@@ -31,3 +31,101 @@ let kiwiIcecream = new Icecream("kiwi");
 let appleIcecream = new Icecream("apple");
 kiwiIcecream; // --> Icecream {flavor: 'kiwi', meltIt: ƒ}
 appleIcecream; // --> Icecream {flavor: 'apple', meltIt: ƒ}
+
+
+class Train {
+    constructor(color, lightsOn) {
+        this.color = color;
+        this.lightsOn = lightsOn;
+    }
+    toggleLights() {
+        this.lightsOn = !this.lightsOn;
+    }
+    lightsStatus() {
+        console.log('Lights on?', this.lightsOn);
+    }
+    getSelf() {
+        console.log(this);
+    }
+    getPrototype() {
+        var proto = Object.getPrototypeOf(this);
+        console.log(proto);
+    }
+}
+
+var train4 = new Train('red', false);
+
+train4.toggleLights(); // undefined
+train4.lightsStatus(); // Lights on? true
+train4.getSelf(); // Train {color: 'red', lightsOn: true}
+train4.getPrototype(); // {constructor: f, toggleLights: f, lightsStatus: f, getSelf: f, getPrototype: f}
+
+
+
+class Cat extends Animal {
+    constructor(sound = 'purr', canJumpHigh = true, canClimbTrees = true, color, energy) {
+        super(color, energy);
+        this.sound = sound;
+        this.canClimbTrees = canClimbTrees;
+        this.canJumpHigh = canJumpHigh;
+    }
+    makeSound() {
+        console.log(this.sound);
+    }
+}
+
+class Bird extends Animal {
+    constructor(sound = 'chirp', canFly = true, color, energy) {
+        super(color, energy);
+        this.sound = sound;
+        this.canFly = canFly;
+    }
+    makeSound() {
+        console.log(this.sound);
+    }
+}
+
+
+class HouseCat extends Cat {
+    constructor(houseCatSound = "meow", sound,canJumpHigh,canClimbTrees, color,energy) {
+        super(sound,canJumpHigh,canClimbTrees, color,energy);
+        this.houseCatSound = houseCatSound;
+    }
+    // makeSound method takes a parameter `option`
+    makeSound(option) {
+        if (option) {
+            super.makeSound(); // Calls the parent class's makeSound if option is true
+        }
+        console.log(this.houseCatSound); // Always prints the HouseCat sound
+    }
+}
+
+class Tiger extends Cat {
+    constructor(tigerSound = "Roar!", sound,canJumpHigh,canClimbTrees, color,energy) {
+        super(sound,canJumpHigh,canClimbTrees, color,energy);
+        this.tigerSound = tigerSound;
+    }
+    // makeSound method takes a parameter `option`
+    makeSound(option) {
+        if (option) {
+            super.makeSound(); // Calls the parent class's makeSound if option is true
+        }
+        console.log(this.tigerSound); // Always prints the Tiger sound
+    }
+}
+
+class Parrot extends Bird {
+    constructor(canTalk = false, sound,canFly, color,energy) {
+        super(sound,canFly, color,energy);
+        this.canTalk = canTalk;
+    }
+    // makeSound method takes a parameter `option`
+    makeSound(option) {
+        if (option) {
+            super.makeSound(); // Calls the parent class's makeSound if option is true
+        }
+        if (this.canTalk) {
+            console.log("I'm a talking parrot!"); // If canTalk is true, it talks
+        }
+    }
+}
